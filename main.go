63fc84999e2f8ad515f4d83f586f4319d23fd4d1
Martin Skatvedt <martinskatvedt@gmail.com>
2022-11-06 19:32:56 +0100

    setup

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"github.com/faiface/pixel"
+	"github.com/faiface/pixel/pixelgl"
+)
+
+func main() {
+	pixelgl.Run(runGame)
+}
+
+func runGame() {
+	cfg := pixelgl.WindowConfig{
+		Title:     "Martins game",
+		Bounds:    pixel.R(0, 0, 1024, 768),
+		VSync:     true,
+		Resizable: true,
+	}
+
+	win, err := pixelgl.NewWindow(cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	win.SetSmooth(false)
+
+	for !win.JustPressed(pixelgl.KeyEscape) {
+		win.Clear(pixel.RGB(0, 0, 0))
+
+		win.Update()
+	}
+}
